18300cf6a6fa8346e711c10fe6a90e6ef050df45
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-12-01 14:41:28 +0900

    modified accession number variable

--- src/main/java/org/glycoinfo/rdf/motif/Motif.java
+++ src/main/java/org/glycoinfo/rdf/motif/Motif.java
@@ -3,5 +3,8 @@ package org.glycoinfo.rdf.motif;
 public interface Motif {
     public static final String MotifURI = "MotifURI";
 	public static final String AccessionNumber = "AccessionNumber";
+	public static final String DeleteAccessionNumber = "DeleteAccessionNumber";
+	public static final String InsertAccessionNumber = "InsertAccessionNumber";
+	public static final String MotifName = "MotifName";
 	public static final String ReducingEnd = "ReducingEnd";
 }
